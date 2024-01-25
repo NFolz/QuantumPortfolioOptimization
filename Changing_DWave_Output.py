53a17d57be479d6e6f616c72393f1d42c33f1d73
Nicolas <Nfolz@uwo.ca>
2024-01-25 15:13:14 -0500

    creating list

--- Changing_DWave_Output.py
+++ Changing_DWave_Output.py
@@ -10,14 +10,10 @@ weightings = [] # Different weightings for each variable
 tickers = ['IBM','MSFT','AAPL','ALPH','META','TSLA'] # All of the tickers we are using (imported from backend, in order of variables)
 
 def createVariableList(tick,weights): # creating the variables that return as bits  
-    variables = {} # dict to hold them
-    tickerCount = 1 # first subscript is the ticker one
+    variables = []
     for ticker in tick:
-        weightCount =1 # second subscript is the weight one
         for weight in weights:
-            variables["X_"+str(tickerCount)+"_"+str(weightCount)] = (ticker,weight) # append the variables dict by adding a new key (variable name) and tuple value (the ticker and the weight)
-            weightCount+=1
-        tickerCount+=1
+            variables+=[str(ticker)+"_"+str(weight)]
     return variables
 
 def main():
@@ -25,13 +21,6 @@ def main():
     weightings = few.findWeights()
     variableList = createVariableList(tickers, weightings)
     print("This is the final variable list: " + str(variableList))
-    
-    variable_key = 'X_3_2'
-    if variable_key in variableList:
-        ticker, weight = variableList[variable_key]
-        print(f"If the quantum results indicate that {variable_key} is 1, then we should buy {ticker} with {weight} of our portfolio.")
-    else:
-        print(f"The variable {variable_key} is not present in the variable list.")
 
 main()
 
