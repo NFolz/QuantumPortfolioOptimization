7830deafc8ad8932fd3947b69cab7e99baaad037
Nicolas <Nfolz@uwo.ca>
2024-01-25 10:35:28 -0500

    big data set test

--- DWave_Portion.py
+++ DWave_Portion.py
@@ -1,7 +1,9 @@
-from dimod.generators import and_gate
-bqm = and_gate('in1', 'in2', 'out')
-
 from dwave.system import DWaveSampler, EmbeddingComposite
 sampler = EmbeddingComposite(DWaveSampler())
-sampleset = sampler.sample(bqm, num_reads=1000)
-print(sampleset)   
+
+linear = {('a', 'a'): -1, ('b', 'b'): -1, ('c', 'c'): -1}
+quadratic = {('a', 'b'): 2, ('b', 'c'): 2, ('a', 'c'): 2}
+Q = {**linear, **quadratic}
+
+sampleset = sampler.sample_qubo(Q, num_reads=5000)
+
